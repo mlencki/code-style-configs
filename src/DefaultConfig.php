ba82dcf7c07ac3ef78b081da492161ea98859035
Mateusz Lencki <mateusz.lencki@gmail.com>
2020-12-29 18:13:27 +0100

    Broken tests

--- src/DefaultConfig.php
+++ src/DefaultConfig.php
@@ -9,6 +9,7 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 class DefaultConfig extends Config
 {
     protected array $sets = [
+        SetList::COMMON,
         SetList::SPACES,
         SetList::ARRAY,
         SetList::DOCBLOCK,
