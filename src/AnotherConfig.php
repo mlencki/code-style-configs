69852802bec8e28918f164fe59a61bc8a8c7d5e9
Mateusz Lencki <mateusz.lencki@gmail.com>
2020-12-29 14:42:04 +0100

    Another config

---
+++ src/AnotherConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MLencki\EcsConfig;
+
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+class AnotherConfig extends Config
+{
+    protected array $sets = [
+        SetList::SPACES,
+        SetList::ARRAY,
+        SetList::DOCBLOCK,
+    ];
+}
